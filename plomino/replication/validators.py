f6bbad06993533e90fe03b8fd0771a281f5f7d92
mamogmx <marco.carbone@gmx.com>
2014-12-19 15:58:36 +0100

    removed validation

--- plomino/replication/validators.py
+++ plomino/replication/validators.py
@@ -2,7 +2,7 @@ import zope.interface
 from z3c.form import validator
 import zope.component
 from sqlalchemy import create_engine
-from plomino.replication.connection import connection
+
 class validateConn(validator.SimpleFieldValidator):
     def validate(self,value):
         try:
@@ -11,8 +11,4 @@ class validateConn(validator.SimpleFieldValidator):
             msg = u"Invalid connection string"
             raise zope.interface.Invalid(msg)
 
-validator.WidgetValidatorDiscriminators(validateConn, field=connection['conn_string'])
 
-# Register the validator so it will be looked up by z3c.form machinery
-
-zope.component.provideAdapter(validateConn)
